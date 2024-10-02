a0e14daf1f677d4677b247a2af7f820e9a9d22ea
julia2095 <julia.men@escola.pr.gov.br>
2024-10-02 14:52:23 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,48 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Fisica',
+    'O que é a lei da convercao da energia?',
+    'não há perda de energia, mas sim uma transformação em energia elétrica',
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quantos minutos tem uma semana inteira?,    
+    '10.800',
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequencia de instruções simples que tem como objetivo obter um resultado. É basicamente uma receita.',
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se pergunta que horas sao em ingles?',
+    'What time is it?',
+)
+
+riaCartao(
+    'biologia',
+    'Qual é a função da biologia?',
+    'usca responder os fenômenos da natureza por meio do método científico, que nos fornece critérios para tal',
+)
+
+criaCartao(
+    'Química',
+    'O que significa Química?',
+    'é uma área da ciência natural que trata principalmente das propriedades das substâncias, as mudanças que elas sofrem, e as leis naturais que descrevem estas mudanças',
+)
+
+criaCartao(
+    'Atronomia',
+    'Por que o espaço é escuro mesmo nas proximidades do Sol?',
+    'A luminosidade azulada que percebemos na Terra de dia é resultado da difusão dos raios solares na atmosfera. A ausência de matéria que exerça função semelhante em outras regiões do espaço torna-o escuro.,
+
+)
+
+criaCartao(
+    'Astronomia',
+    'Um buraco negro pode engolir outro?',
+    'Teoricamente, não existem limites para a massa que os buracos negros podem engolir. Portanto, eles poderiam absorver matéria indefinidamente. Um buraco negro não pode engolir outro, mas eles podem se unir, formando buracos negros ainda maiores'
 )
